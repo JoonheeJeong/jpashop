e8892d5a7cf0ed9f11cf74c7c36f38736d39c540
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-25 14:08:37 +0900

    feat: 회원 목록 조회 구현 (#2)

--- src/main/java/jpabook/jpashop/repository/JpaMemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/JpaMemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -23,5 +24,11 @@ public class JpaMemberRepository implements MemberRepository {
     public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 }
