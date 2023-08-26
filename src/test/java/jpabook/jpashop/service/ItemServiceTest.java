3528a3fe75c7497b62739fe0d63a7e35e89a6e42
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-26 22:06:35 +0900

    test: 상품 목록 조회 서비스 테스트 (#3)

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,6 +40,34 @@ class ItemServiceTest {
         assertThat(byId.get()).isInstanceOf(Book.class);
         Book book = (Book) byId.get();
         assertThat(book.getName()).isEqualTo(dto.getName());
+    }
+
+    @DisplayName("상품 목록 조회")
+    @Test
+    void when3BookExist_thenFoundThem() {
+        // given
+        List<BookRegisterDTO> dtos = List.of(
+                newBookRegisterDTO("루터 선집", "마르틴 루터", "1234"),
+                newBookRegisterDTO("이방인의 염려", "쇠얀 케르케고르", "1235"),
+                newBookRegisterDTO("순전한 기독교", "C. S. 루이스", "1236")
+        );
+        dtos.forEach(sut::register);
+
+        // when
+        List<Item> items = sut.getList();
+
+        // then
+        assertThat(items).hasSize(dtos.size());
+        IntStream.range(0, dtos.size())
+                .forEach(i -> {
+                    Item item = items.get(i);
+                    assertThat(item).isInstanceOf(Book.class);
+                    Book book = (Book) item;
+                    BookRegisterDTO dto = dtos.get(i);
+                    assertThat(book.getName()).isEqualTo(dto.getName());
+                    assertThat(book.getAuthor()).isEqualTo(dto.getAuthor());
+                    assertThat(book.getIsbn()).isEqualTo(dto.getIsbn());
+                });
     }
 
     private BookRegisterDTO newBookRegisterDTO(String name, String author, String isbn) {
