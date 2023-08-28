92cca7b8dd755009baa831692c83cb4b72826184
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-28 12:48:14 +0900

    feat: 배송 엔티티 생성자 구현 (#4)
    - 엔티티 조건에 의해 무인자 생성자 등록

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.type.DeliveryStatus;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Delivery {
@@ -20,7 +23,11 @@ public class Delivery {
     private Address address;
 
     @Enumerated(EnumType.STRING)
-    private DeliveryStatus status;
+    private DeliveryStatus status = DeliveryStatus.READY;
+
+    public Delivery(Address address) {
+        this.address = address;
+    }
 
     public void setOrder(Order order) {
         this.order = order;
