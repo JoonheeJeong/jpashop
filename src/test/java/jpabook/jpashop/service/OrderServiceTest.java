435b7555666f3dd59a4806595b9959a92b0aba84
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-28 14:34:59 +0900

    test: 주문 취소 서비스 테스트 (#4)

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.type.OrderStatus;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
@@ -79,6 +80,47 @@ class OrderServiceTest {
                     assertThat(item).isSameAs(books.get(i));
                     assertThat(orderItem.getQuantity()).isEqualTo(quantities.get(i));
                     assertThat(stockQuantitiesOriginal.get(i)).isEqualTo(item.getStockQuantity() + orderItem.getQuantity());
+                });
+    }
+
+    @DisplayName("주문 취소")
+    @Test
+    void whenCancelOrder_thenStatusCancelledAndStockRestored() {
+        // given
+        Member member = MemberUtil.newMember("정준희");
+        memberRepository.save(member);
+
+        List<Book> books = List.of(
+                ItemUtil.newBook("루터 선집", "마르틴 루터", "1234"),
+                ItemUtil.newBook("이방인의 염려", "쇠얀 케르케고르", "1235"),
+                ItemUtil.newBook("순전한 기독교", "C. S. 루이스", "1236")
+        );
+        List<Integer> stockQuantitiesOriginal = books.stream()
+                .map(Item::getStockQuantity)
+                .collect(Collectors.toList());
+        books.forEach(itemRepository::save);
+
+        List<Long> itemIds = books.stream()
+                .map(Item::getId)
+                .collect(Collectors.toList());
+        List<Integer> quantities = List.of(2, 3, 1);
+        OrderRequestDTO dto = new OrderRequestDTO(member.getId(), itemIds, quantities);
+
+        Long orderId = orderService.order(dto);
+
+        // when
+        orderService.cancelOrder(orderId);
+
+        // then
+        Order order = orderRepository.findById(orderId).orElseThrow();
+        assertThat(order.getStatus()).isSameAs(OrderStatus.CANCEL);
+        List<OrderItem> orderItemsFound = order.getOrderItems();
+        assertThat(orderItemsFound).hasSize(books.size());
+        IntStream.range(0, books.size())
+                .forEach(i -> {
+                    Integer stockQuantity = orderItemsFound.get(i).getItem().getStockQuantity();
+                    Integer expected = stockQuantitiesOriginal.get(i);
+                    assertThat(stockQuantity).isEqualTo(expected);
                 });
     }
 }
