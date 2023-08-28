b769805d4a6f2569c13e8e1c8e59d7beabde3a06
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-28 12:39:44 +0900

    fix: 주문 엔티티에서 주문상품, 배송 cascade (#4)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -26,10 +26,10 @@ public class Order {
     private Member member;
 
     @Builder.Default
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
